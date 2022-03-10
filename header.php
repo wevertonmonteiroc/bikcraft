4cbddb7272dd6a8e5b669b0dc7d15a70e6aeb132
wevertonmonteiroc <wevertonmonteiroc@gmail.com>
2022-03-09 23:08:06 -0300

    Criação das paginas header.php, footer.php; adição das funções get header e get footer, para pegar o cabeçalho e rodapé respectivamente, adição da função bloginfo name no titulo e ogtitle da pagina header.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title><?php bloginfo('name'); ?></title>
+		<meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
+
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="<?php bloginfo('name'); ?>"/>
+		<meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
+		<meta property="og:url" content="http://bikcraft.com"/>
+		<meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="shortcut icon" href="favicon.ico">
+
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+		<script>document.documentElement.classList.add("js");</script>
+
+        <!-- Inicio do Wordpress Header -->
+        <?php wp_head(); ?>
+        <!-- Final do Wordpress Header -->
+	</head>
+	<body>
+
+		<header class="header">
+			<div class="container">
+				<a href="index.html" class="grid-4">
+					<img src="img/bikcraft.svg" alt="Bikcraft">
+				</a>
+				<nav class="grid-12 header_menu">
+					<ul>
+						<li><a href="sobre.html">Sobre</a></li>
+						<li><a href="produtos.html">Produtos</a></li>
+						<li><a href="portfolio.html">Portfólio</a></li>
+						<li><a href="contato.html">Contato</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
--- index.php
+++ index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title>Bikcraft - Bicicletas Personalizadas</title>
-		<meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
-
-		<meta property="og:type" content="website"/>
-		<meta property="og:title" content="Bikcraft - Bicicletas Personalizadas"/>
-		<meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
-		<meta property="og:url" content="http://bikcraft.com"/>
-		<meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
-
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<link rel="shortcut icon" href="favicon.ico">
-
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-		<script>document.documentElement.classList.add("js");</script>
-	</head>
-	<body>
-
-		<header class="header">
-			<div class="container">
-				<a href="index.html" class="grid-4">
-					<img src="img/bikcraft.svg" alt="Bikcraft">
-				</a>
-				<nav class="grid-12 header_menu">
-					<ul>
-						<li><a href="sobre.html">Sobre</a></li>
-						<li><a href="produtos.html">Produtos</a></li>
-						<li><a href="portfolio.html">Portfólio</a></li>
-						<li><a href="contato.html">Contato</a></li>
-					</ul>
-				</nav>
-			</div>
-		</header>
+<?php get_header(); ?>
 
 		<section class="introducao">
 			<div class="container">
@@ -123,54 +87,4 @@
 			</div>
 		</section>
 
-		<section class="quebra">
-			<blockquote class="quote-externo container">
-				<p>“o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.”</p>
-				<cite>WILLIAM MORRIS</cite>
-			</blockquote>
-		</section>
-
-		<footer>
-			<div class="footer">
-				<div class="container">
-
-					<div class="grid-8 footer_historia">
-						<h3>Nossa História</h3>
-						<p>O verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana cotidiana. interesse por todos os detalhes da vida cotidiana cotidiana.</p>
-					</div>
-
-					<div class="grid-4 footer_contato">
-						<h3>Contato</h3>
-						<ul>
-							<li>- 21 23232-4343</li>
-							<li>- contato@bikcraft.com</li>
-							<li>- Botafago - RJ</li>
-						</ul>
-					</div>
-
-					<div class="grid-4 footer_redes">
-						<h3>Contato</h3>
-						<ul>
-							<li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.svg" alt="Facebook Bikcraft"></a></li>
-							<li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.svg" alt="Instagram Bikcraft"></a></li>
-							<li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.svg" alt="Twitter Bikcraft"></a></li>
-						</ul>
-					</div>
-
-				</div>
-			</div>
-
-			<div class="copy">
-				<div class="container">
-					<p class="grid-16">Bikcraft 2019 - Alguns direitos reservados.</p>
-				</div>
-			</div>
-		</footer>
-
-	<!-- JavaScript -->
-	<script src="./js/simple-anime.js"></script>
-  <script src="./js/script.js"></script>
-	<!-- JavaScript -->
-
-	</body>
-</html>
+<?php get_footer(); ?>
